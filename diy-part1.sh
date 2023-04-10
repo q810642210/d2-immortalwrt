a51dbec7fe45a64e7228259535d8429be989969c
xgitl <69244012+xgitl@users.noreply.github.com>
2023-04-10 22:42:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 # echo 'src-svn adbyby_plus https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-adbyby-plus' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
-# git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
