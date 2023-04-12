d474342cde051437b1625d074a61f4d0e6c9566e
xgitl <69244012+xgitl@users.noreply.github.com>
2023-04-13 00:03:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,14 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*passwall\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*xray-core\)/\1/' feeds.conf.default
+
+
+sed -i '/^src-git/d' feeds.conf.default
+echo 'src-git packages https://github.com/immortalwrt/packages.git' >> feeds.conf.default
+echo 'src-git luci https://github.com/immortalwrt/luci.git' >> feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing.git' >> feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
+
 
 
 # Add a feed source
