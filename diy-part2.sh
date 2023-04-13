38cd41b67bd04f3af6096f8d9d82877ae955982b
644234041 <123048795+644234041@users.noreply.github.com>
2023-04-13 20:44:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 #修改主机名
 sed -i 's/ImmortalWrt/Openwrt/g' package/base-files/files/bin/config_generate
