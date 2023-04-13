cc70e9e06ba44e7664cf26a479cb82b8f1649e33
xgitl <69244012+xgitl@users.noreply.github.com>
2023-04-13 10:48:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 
 
-# sed -i '/^src-git/d' feeds.conf.default
+sed -i '/^src-git luci/d' feeds.conf.default
 # echo 'src-git packages https://github.com/immortalwrt/packages.git' >> feeds.conf.default
 # echo 'src-git routing https://github.com/openwrt/routing.git' >> feeds.conf.default
 # echo 'src-git telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
